f36cdb3c59e47fb2f1e6a38a780b56d44669bb89
Kondr11 <alexk1201@gmail.com>
2020-01-23 22:57:47 +0400

    2.0.3

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-files=`find . -name "*.cpp" -or -name "*.hpp" -or -name ".h" | grep -v "./tools/*" -or -v "./cmake-build-debug/CMakeFiles/3.15.3/CompilerIdCXX/CMakeCXXCompilerId.cpp" -or -v "./tests" -or -v "./include/1/"`
+files=`find . -name "*.cpp" -or -name "*.hpp" -or -name ".h" | grep -v "./tools/*" | grep -v "./cmake-build-debug/CMakeFiles/3.15.3/CompilerIdCXX/CMakeCXXCompilerId.cpp" | grep -v "./tests" | grep -v "./include/1/"`
 filter=-build/c++11,-runtime/references,-whitespace/braces,-whitespace/indent,-whitespace/comments,-build/include_order
 echo $files | xargs cpplint --filter=$filter
 
