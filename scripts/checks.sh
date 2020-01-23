e6131a0dc88ff6b19154cf7c6eb5e9b0eaf723a5
Kondr11 <alexk1201@gmail.com>
2020-01-23 20:31:46 +0400

    1.3.5

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-files=`find . -name "*.cpp" -or -name "*.hpp" -or -name ".h" -not -name "*build*/**,tests,include/1/**" | grep -v "./tools/*"`
+files=`find . -name "*.cpp" -or -name "*.hpp" -or -name ".h" | grep -v "./tools/*" -or -v "./*build*/**" -or -v "./tests" -or -v "./include/1/**"`
 filter=-build/c++11,-runtime/references,-whitespace/braces,-whitespace/indent,-whitespace/comments,-build/include_order
 echo $files | xargs cpplint --filter=$filter
 
