acc1d1c4c600c305cf3c32a8239fccd0418aa166
Kondr11 <55596665+Kondr11@users.noreply.github.com>
2020-09-17 21:10:05 +0300

    Update source.cpp

--- sources/source.cpp
+++ sources/source.cpp
@@ -1,20 +1,15 @@
-// Copyright 2020 Kondr11
-
-#include "Enumeration.hpp"
-
 void enumeration() {
-    static const std::string str = "0000";
-    while (true) {
-        std::string rand = std::to_string(std::rand());
-        std::string hash = picosha2::hash256_hex_string(rand);
+    static const std::string str = "0000";//строка 4 нуля по заданию
+    while (true) {//бесконечнфй цикл
+        std::string rand = std::to_string(std::rand());//создаем рандомную строку
+        std::string hash = picosha2::hash256_hex_string(rand);//получаем хеш этой строки
 
-        if (hash.substr(hash.size() - str.size()) == str) {
+        if (hash.substr(hash.size() - str.size()) == str) {//если строка оканчивается на 4 нуля то
             BOOST_LOG_TRIVIAL(info) << "Found correct value '" << rand
-            << "' with hash '" << hash << "'";
+            << "' with hash '" << hash << "'";//выводим в лог с уровнем инфо, который попадет в консоль
         } else {
-            BOOST_LOG_TRIVIAL(trace) << "Current value '" << rand
+            BOOST_LOG_TRIVIAL(trace) << "Current value '" << rand//иначе выводим в лог который не попадет в консоль
             << "' with hash '" << hash << "'";
         }
     }
 }
-
